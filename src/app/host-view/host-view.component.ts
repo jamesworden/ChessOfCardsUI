b69e82149718c72c705d0deb2006e579cbb191e2
jamesworden <jamesworden.ny@gmail.com>
2022-08-13 15:46:17 -0400

    LAN-4: host view has game code displayed

--- src/app/host-view/host-view.component.ts
+++ src/app/host-view/host-view.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-host-view',
   templateUrl: './host-view.component.html',
-  styleUrls: ['./host-view.component.css']
+  styleUrls: ['./host-view.component.css'],
 })
 export class HostViewComponent implements OnInit {
+  @Input() gameCode: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
