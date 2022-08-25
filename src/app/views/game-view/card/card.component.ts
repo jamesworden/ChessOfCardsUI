c12c49fd83f70f564f2adbac0a8d97b8830151bf
jamesworden <jamesworden.ny@gmail.com>
2022-08-25 12:58:44 -0400

    Scaffoled drag drop logic

--- src/app/views/game-view/card/card.component.ts
+++ src/app/views/game-view/card/card.component.ts
@@ -1,3 +1,4 @@
+import { DragRef, Point } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +6,22 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() cardImageFileName: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  onDragReleased() {
+    // TODO: Check if target pos is empty space or card and if they can play that move or not
+    // and then call a service to play that move?
+  }
+
+  constrainPosition(point: Point, dragRef: DragRef): Point {
+    // TODO: constrain to board area
+
+    return {
+      x: 0,
+      y: 0,
+    };
+  }
 }
