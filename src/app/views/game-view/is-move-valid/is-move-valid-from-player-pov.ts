a398f2da1727f0f003c2031cc667c110e0f837ed
jamesworden <jamesworden.ny@gmail.com>
2022-09-01 21:32:03 -0400

    More game rules

--- src/app/views/game-view/is-move-valid/is-move-valid-from-player-pov.ts
+++ src/app/views/game-view/is-move-valid/is-move-valid-from-player-pov.ts
@@ -1,11 +1,57 @@
+import { LaneAdvantageModel } from 'src/app/models/lane-advantage.model';
 import { LaneModel } from 'src/app/models/lane.model';
 import { MoveModel } from 'src/app/models/move.model';
+import { PlayedByModel } from 'src/app/models/played-by.model';
+import { allPreviousRowsOccupied } from './valid-move-checks';
+import { cardsHaveMatchingSuitOrKind } from './valid-move-checks/cards-have-matching-suit-or-kind';
+import { getTopCardOnTargetRow } from './valid-move-checks/get-top-card-on-target-row';
 
 export function isMoveValidFromPlayerPov(lane: LaneModel, move: MoveModel) {
   const { LaneAdvantage, LastCardPlayed, Rows } = lane;
   const { Card, TargetRowIndex } = move;
 
-  console.log(lane, move);
+  const cardsInLane = !!LastCardPlayed;
+  const topCardOnTargetRow = getTopCardOnTargetRow(lane, TargetRowIndex);
+
+  const moveIsPlayerSide = TargetRowIndex < 3;
+  const moveIsMiddle = TargetRowIndex === 3;
+  const moveIsOpponentSide = TargetRowIndex > 3;
+
+  const playerHasAdvantage = LaneAdvantage === LaneAdvantageModel.Player;
+  const opponentHasAdvantage = LaneAdvantage === LaneAdvantageModel.Opponent;
+  const noAdvantage = LaneAdvantage === LaneAdvantageModel.None;
+
+  if (moveIsMiddle) {
+    return false;
+  }
+
+  if (moveIsPlayerSide && playerHasAdvantage) {
+    return false;
+  }
+
+  if (moveIsOpponentSide && opponentHasAdvantage) {
+    return false;
+  }
+
+  if (moveIsOpponentSide && noAdvantage) {
+    return false;
+  }
+
+  if (moveIsPlayerSide && !allPreviousRowsOccupied(lane, TargetRowIndex)) {
+    return false;
+  }
+
+  if (cardsInLane && !cardsHaveMatchingSuitOrKind(Card, LastCardPlayed!)) {
+    return false;
+  }
+
+  // if (cardsInLane && topCardOnTargetRow.PlayedBy === PlayedByModel. )
 
   return true;
 }
+
+// if (noCardsInLane) {
+//   return TargetRowIndex === 0
+// } else {
+//   return cardsHaveMatchingSuitOrKind(Card, LastCardPlayed!) && allPreviousRowsOccupied(lane, TargetRowIndex) &&
+// }
--- src/app/views/game-view/is-move-valid/valid-move-checks/index.ts
+++ src/app/views/game-view/is-move-valid/valid-move-checks/index.ts
@@ -1,3 +1,2 @@
-export * from './is-players-turn';
 export * from './all-previous-rows-occupied';
 export * from './all-following-rows-occupied';
