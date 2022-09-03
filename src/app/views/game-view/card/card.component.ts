3ce75b3e9cee7dd23189099d8dcb84dbda845f72
jamesworden <jamesworden.ny@gmail.com>
2022-09-03 14:07:59 -0400

    Middle card rotates properly!

--- src/app/views/game-view/card/card.component.ts
+++ src/app/views/game-view/card/card.component.ts
@@ -1,15 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PlayedByModel } from 'src/app/models/played-by.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
+export class CardComponent implements OnChanges {
   @Input() cardImageFileName: string;
   @Input() playerCanDrag = false;
   @Input() suit: string;
   @Input() kind: string;
+  @Input() isMiddleCard: boolean;
+  @Input() isPlayedBy: PlayedByModel;
+  @Input() isHost: boolean;
+
+  tiltDegrees = 0;
 
   constructor() {}
+
+  ngOnChanges() {
+    const tiltRight =
+      (this.isHost &&
+        this.isMiddleCard &&
+        this.isPlayedBy === PlayedByModel.Host) ||
+      (!this.isHost &&
+        this.isMiddleCard &&
+        this.isPlayedBy === PlayedByModel.Host);
+
+    const tiltLeft =
+      (this.isHost &&
+        this.isMiddleCard &&
+        this.isPlayedBy === PlayedByModel.Guest) ||
+      (!this.isHost &&
+        this.isMiddleCard &&
+        this.isPlayedBy === PlayedByModel.Guest);
+
+    if (tiltRight) {
+      this.tiltDegrees = 45;
+    } else if (tiltLeft) {
+      this.tiltDegrees = -45;
+    }
+  }
 }
