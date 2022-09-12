f233ce9b03f1f5f624a1053d0622e8438a79f8a1
jamesworden <jamesworden.ny@gmail.com>
2022-09-11 20:17:51 -0400

    More unit

--- projects/client/src/app/views/game-view/is-move-valid/move-checks/started-move-player-side-and-place-card-out-of-order.spec.ts
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/started-move-player-side-and-place-card-out-of-order.spec.ts
@@ -37,6 +37,104 @@ describe('[Move Check]: started move player side and place card out of order', (
 
     expect(result).toBe(false);
   });
+
+  it('should return true when hosts first move is on their second position and the first position has a guest card', () => {
+    // Note that the target row index is 1.
+    const move: MoveModel = {
+      PlaceCardAttempts: [
+        {
+          Card: {
+            Kind: KindModel.Ace,
+            Suit: SuitModel.Spades,
+            PlayedBy: PlayerOrNoneModel.Host,
+          },
+          TargetLaneIndex: 0,
+          TargetRowIndex: 1,
+        },
+      ],
+    };
+
+    // Note that the top, or last card of the first row is played by guest.
+    // Note that IsHost is true.
+    const partialTestGameState: Partial<PlayerGameStateModel> = {
+      Lanes: [
+        {
+          LaneAdvantage: PlayerOrNoneModel.None,
+          Rows: [
+            [
+              {
+                Kind: KindModel.Ace,
+                Suit: SuitModel.Spades,
+                PlayedBy: PlayerOrNoneModel.Host,
+              },
+              {
+                Kind: KindModel.Ace,
+                Suit: SuitModel.Spades,
+                PlayedBy: PlayerOrNoneModel.Guest,
+              },
+            ],
+          ],
+          WonBy: PlayerOrNoneModel.None,
+        },
+      ],
+      IsHost: true,
+    };
+  });
+
+  it('should return true when guests first move is on their second position and the first position has a host card', () => {
+    // Note that the target row index is 5.
+    const move: MoveModel = {
+      PlaceCardAttempts: [
+        {
+          Card: {
+            Kind: KindModel.Ace,
+            Suit: SuitModel.Spades,
+            PlayedBy: PlayerOrNoneModel.Host,
+          },
+          TargetLaneIndex: 0,
+          TargetRowIndex: 5,
+        },
+      ],
+    };
+
+    // Note that the top, or last card of the first row is played by host.
+    // Note that IsHost is false.
+    const partialTestGameState: Partial<PlayerGameStateModel> = {
+      Lanes: [
+        {
+          LaneAdvantage: PlayerOrNoneModel.None,
+          Rows: [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [
+              {
+                Kind: KindModel.Ace,
+                Suit: SuitModel.Spades,
+                PlayedBy: PlayerOrNoneModel.Guest,
+              },
+              {
+                Kind: KindModel.Ace,
+                Suit: SuitModel.Spades,
+                PlayedBy: PlayerOrNoneModel.Host,
+              },
+            ],
+          ],
+          WonBy: PlayerOrNoneModel.None,
+        },
+      ],
+      IsHost: false,
+    };
+
+    const gameState = partialTestGameState as PlayerGameStateModel;
+
+    const result = startedMovePlayerSideAndPlaceCardOutOfOrder(gameState, move);
+
+    expect(result).toBe(true);
+  });
 });
 
 function getTestResult(isHost: boolean, targetRowIndex: number) {
