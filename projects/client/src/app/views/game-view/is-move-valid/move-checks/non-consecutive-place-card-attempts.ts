129aaeaba9ea248021a7607d70d5d160c05ca1d2
jamesworden <jamesworden.ny@gmail.com>
2022-09-12 18:08:25 -0400

    more

---
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/non-consecutive-place-card-attempts.ts
@@ -0,0 +1,7 @@
+import { MoveModel } from 'projects/client/src/app/models/move.model';
+
+export function nonConsecutivePlaceCardAttempts(move: MoveModel) {
+  // const {PlaceCardAttempts} = move
+  // const sortedByTargetLaneIndex = PlaceCardAttempts.sort((a, b) => a.TargetLaneIndex - b.TargetLaneIndex)
+  return false;
+}
