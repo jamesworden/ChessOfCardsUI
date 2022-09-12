fcb14ebd6873005b2de5abff6a7b50517a669418
jamesworden <jamesworden.ny@gmail.com>
2022-09-11 20:43:06 -0400

    refactoed unit test

--- projects/client/src/app/views/game-view/is-move-valid/move-checks/move-on-different-lanes.spec.ts
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/move-on-different-lanes.spec.ts
@@ -1,52 +1,49 @@
-import { KindModel } from 'projects/client/src/app/models/kind.model';
-import { MoveModel } from 'projects/client/src/app/models/move.model';
-import { PlaceCardAttemptModel } from 'projects/client/src/app/models/place-card-attempt.model';
-import { PlayerOrNoneModel } from 'projects/client/src/app/models/player-or-none-model';
-import { SuitModel } from 'projects/client/src/app/models/suit.model';
+import { MoveBuilder } from '../testing/move-builder';
+import { PlaceCardAttemptBuilder } from '../testing/place-card-attempt-builder';
 import { moveOnDifferentLanes } from './move-on-different-lanes';
 
 describe('[Move Check]: move on different lanes', () => {
   it('should return true when move has place card attempts on different target lanes', () => {
-    const move = getTestMove();
-    const laneIndexZero = getTestPlaceCardAttemptOnLane(0);
-    const laneIndexOne = getTestPlaceCardAttemptOnLane(1);
-    const laneIndexTwo = getTestPlaceCardAttemptOnLane(2);
-    move.PlaceCardAttempts.push(laneIndexZero, laneIndexOne, laneIndexTwo);
-    const result = moveOnDifferentLanes(move);
+    const laneIndexZero = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(0)
+      .build();
 
-    expect(result).toBe(true);
+    const laneIndexOne = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(1)
+      .build();
+
+    const laneIndexTwo = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(2)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(laneIndexZero)
+      .addPlaceCardAttempt(laneIndexOne)
+      .addPlaceCardAttempt(laneIndexTwo)
+      .build();
+
+    expect(moveOnDifferentLanes(move)).toBe(true);
   });
 
   it('should return false when move has place card attempts on only one target lane', () => {
-    const move = getTestMove();
-    const laneIndexZero1 = getTestPlaceCardAttemptOnLane(0);
-    const laneIndexZero2 = getTestPlaceCardAttemptOnLane(0);
-    const laneIndexZero3 = getTestPlaceCardAttemptOnLane(0);
-    move.PlaceCardAttempts.push(laneIndexZero1, laneIndexZero2, laneIndexZero3);
-    const result = moveOnDifferentLanes(move);
+    const laneIndexZero = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(3)
+      .build();
 
-    expect(result).toBe(false);
+    const laneIndexOne = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(3)
+      .build();
+
+    const laneIndexTwo = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(3)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(laneIndexZero)
+      .addPlaceCardAttempt(laneIndexOne)
+      .addPlaceCardAttempt(laneIndexTwo)
+      .build();
+
+    expect(moveOnDifferentLanes(move)).toBe(false);
   });
 });
-
-function getTestMove() {
-  const move: MoveModel = {
-    PlaceCardAttempts: [],
-  };
-
-  return move;
-}
-
-function getTestPlaceCardAttemptOnLane(TargetLaneIndex: number) {
-  const placeCardAttempt: PlaceCardAttemptModel = {
-    Card: {
-      Kind: KindModel.Ace,
-      Suit: SuitModel.Spades,
-      PlayedBy: PlayerOrNoneModel.Host,
-    },
-    TargetLaneIndex,
-    TargetRowIndex: 0,
-  };
-
-  return placeCardAttempt;
-}
