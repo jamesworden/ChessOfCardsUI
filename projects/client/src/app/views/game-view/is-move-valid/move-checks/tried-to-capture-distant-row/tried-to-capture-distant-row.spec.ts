3dddfe48e1e1889c958b7eb19f37fc85991e0095
jamesworden <jamesworden.ny@gmail.com>
2022-09-14 14:24:15 -0400

    unit

--- projects/client/src/app/views/game-view/is-move-valid/move-checks/tried-to-capture-distant-row/tried-to-capture-distant-row.spec.ts
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/tried-to-capture-distant-row/tried-to-capture-distant-row.spec.ts
@@ -34,15 +34,65 @@ describe('[Move Check]: tried to capture distant row', () => {
     expect(triedToCaptureDistantRow(gameState, move)).toBeTruthy();
   });
 
-  it(
-    'should return true if host played second position with no first position'
-  );
-  it(
-    'should return true if guest played second position with no first position'
-  );
+  it('should return true if host played second position with no first position', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(1)
+      .setTargetLaneIndex(0)
+      .build();
 
-  it('should return false if host played first guest side position');
-  it('should return false if guest played first host side position');
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(true).build();
+
+    expect(triedToCaptureDistantRow(gameState, move)).toBeFalsy();
+  });
+
+  it('should return true if guest played second position with no first position', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(5)
+      .setTargetLaneIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(false).build();
+
+    expect(triedToCaptureDistantRow(gameState, move)).toBeFalsy();
+  });
+
+  it('should return false if host played first guest side position', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(6)
+      .setTargetLaneIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(true).build();
+
+    expect(triedToCaptureDistantRow(gameState, move)).toBeFalsy();
+  });
+
+  it('should return false if guest played first host side position', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(0)
+      .setTargetLaneIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(false).build();
+
+    expect(triedToCaptureDistantRow(gameState, move)).toBeFalsy();
+  });
 
   it(
     'should return true if host tried to capture second position when guest has first position'
