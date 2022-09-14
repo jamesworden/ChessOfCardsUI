e75330f6edbfdc1b286a2d2aee0c75ce57393cc3
jamesworden <jamesworden.ny@gmail.com>
2022-09-13 23:11:16 -0400

    unit

--- projects/client/src/app/views/game-view/is-move-valid/move-checks/player-has-advantage.spec.ts
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/player-has-advantage.spec.ts
@@ -21,9 +21,54 @@ describe('[Move Check]: player has advantage', () => {
     expect(playerHasAdvantage(gameState, move)).toBeTruthy();
   });
 
-  it('should return false when player is host and guest has lane advantage', () => {});
+  it('should return false when player is host and guest has lane advantage', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(0)
+      .build();
 
-  it('should return true when player is guest and guest has lane advantage', () => {});
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
 
-  it('should return false when player is guest and host has lane advantage', () => {});
+    const gameState = new GameStateBuilder()
+      .setGuestLaneAdvantage(0)
+      .setIsHost(true)
+      .build();
+
+    expect(playerHasAdvantage(gameState, move)).toBeFalsy();
+  });
+
+  it('should return true when player is guest and guest has lane advantage', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder()
+      .setGuestLaneAdvantage(0)
+      .setIsHost(false)
+      .build();
+
+    expect(playerHasAdvantage(gameState, move)).toBeTruthy();
+  });
+
+  it('should return false when player is guest and host has lane advantage', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetLaneIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder()
+      .setHostLaneAdvantage(0)
+      .setIsHost(false)
+      .build();
+
+    expect(playerHasAdvantage(gameState, move)).toBeFalsy();
+  });
 });
