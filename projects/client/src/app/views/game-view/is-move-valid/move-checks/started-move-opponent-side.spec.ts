1e44931ecb7acccee03667b9c1aca56719372375
jamesworden <jamesworden.ny@gmail.com>
2022-09-13 22:22:48 -0400

    unit

--- projects/client/src/app/views/game-view/is-move-valid/move-checks/started-move-opponent-side.spec.ts
+++ projects/client/src/app/views/game-view/is-move-valid/move-checks/started-move-opponent-side.spec.ts
@@ -1,38 +1,10 @@
 import { GameStateBuilder } from '../testing/game-state-builder';
 import { MoveBuilder } from '../testing/move-builder';
 import { PlaceCardAttemptBuilder } from '../testing/place-card-attempt-builder';
-import { startedMovePlayerSide } from './started-move-player-side';
+import { startedMoveOpponentSide } from './started-move-opponent-side';
 
-describe('[Move Check]: started move player side', () => {
-  it('should return true when host moved player side', () => {
-    const placeCardAttempt = new PlaceCardAttemptBuilder()
-      .setTargetRowIndex(0)
-      .build();
-
-    const move = new MoveBuilder()
-      .addPlaceCardAttempt(placeCardAttempt)
-      .build();
-
-    const gameState = new GameStateBuilder().setIsHost(true).build();
-
-    expect(startedMovePlayerSide(gameState, move)).toBe(true);
-  });
-
-  it('should return true when guest moved player side', () => {
-    const placeCardAttempt = new PlaceCardAttemptBuilder()
-      .setTargetRowIndex(6)
-      .build();
-
-    const move = new MoveBuilder()
-      .addPlaceCardAttempt(placeCardAttempt)
-      .build();
-
-    const gameState = new GameStateBuilder().setIsHost(false).build();
-
-    expect(startedMovePlayerSide(gameState, move)).toBe(true);
-  });
-
-  it('should return false when host moved opponent side', () => {
+describe('[Move Check]: started move opponent side', () => {
+  it('should return true when host moved guest side', () => {
     const placeCardAttempt = new PlaceCardAttemptBuilder()
       .setTargetRowIndex(6)
       .build();
@@ -43,10 +15,24 @@ describe('[Move Check]: started move player side', () => {
 
     const gameState = new GameStateBuilder().setIsHost(true).build();
 
-    expect(startedMovePlayerSide(gameState, move)).toBe(false);
+    expect(startedMoveOpponentSide(gameState, move)).toBe(true);
   });
 
-  it('should return false when guest moved opponent side', () => {
+  it('should return false when host moved host side', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(0)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(true).build();
+
+    expect(startedMoveOpponentSide(gameState, move)).toBe(false);
+  });
+
+  it('should return true when guest moved host side', () => {
     const placeCardAttempt = new PlaceCardAttemptBuilder()
       .setTargetRowIndex(0)
       .build();
@@ -57,6 +43,20 @@ describe('[Move Check]: started move player side', () => {
 
     const gameState = new GameStateBuilder().setIsHost(false).build();
 
-    expect(startedMovePlayerSide(gameState, move)).toBe(false);
+    expect(startedMoveOpponentSide(gameState, move)).toBe(true);
+  });
+
+  it('should return false when guest moved guest side', () => {
+    const placeCardAttempt = new PlaceCardAttemptBuilder()
+      .setTargetRowIndex(6)
+      .build();
+
+    const move = new MoveBuilder()
+      .addPlaceCardAttempt(placeCardAttempt)
+      .build();
+
+    const gameState = new GameStateBuilder().setIsHost(false).build();
+
+    expect(startedMoveOpponentSide(gameState, move)).toBe(false);
   });
 });
