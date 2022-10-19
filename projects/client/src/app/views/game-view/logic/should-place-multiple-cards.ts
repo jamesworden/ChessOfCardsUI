8e5812a032977f6e766b2e2f99fdb935fd68839d
jamesworden <jamesworden.ny@gmail.com>
2022-10-18 21:07:14 -0400

    update comment for util fn

--- projects/client/src/app/views/game-view/logic/should-place-multiple-cards.ts
+++ projects/client/src/app/views/game-view/logic/should-place-multiple-cards.ts
@@ -1,18 +1,22 @@
 import { PlaceCardAttemptModel } from '../../../models/place-card-attempt.model';
 import { PlayerGameStateModel } from '../../../models/player-game-state-model';
 
+/**
+ * @returns true if the first place card attempt is in a position where multiple
+ * cards can be played after it, otherwise false.
+ */
 export function shouldPlaceMultipleCards(
-  placeCardAttempt: PlaceCardAttemptModel,
+  firstPlaceCardAttempt: PlaceCardAttemptModel,
   latestGameStateSnapshot: PlayerGameStateModel
 ) {
   const { IsHost, Hand } = latestGameStateSnapshot;
-  const defendingAsHost = IsHost && placeCardAttempt.TargetRowIndex < 3;
-  const defendingAsGuest = !IsHost && placeCardAttempt.TargetRowIndex > 3;
+  const defendingAsHost = IsHost && firstPlaceCardAttempt.TargetRowIndex < 3;
+  const defendingAsGuest = !IsHost && firstPlaceCardAttempt.TargetRowIndex > 3;
   const isDefensiveMove = defendingAsHost || defendingAsGuest;
 
   const hasOtherPotentialPairCards = Hand.Cards.some((card) => {
-    const suitNotMatch = card.Suit != placeCardAttempt.Card.Suit;
-    const kindMatches = card.Kind === placeCardAttempt.Card.Kind;
+    const suitNotMatch = card.Suit != firstPlaceCardAttempt.Card.Suit;
+    const kindMatches = card.Kind === firstPlaceCardAttempt.Card.Kind;
 
     return suitNotMatch && kindMatches;
   });
