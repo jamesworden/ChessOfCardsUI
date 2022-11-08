ce631afcb0a97d363827a43070de67b026cc8ab3
jamesworden <jamesworden.ny@gmail.com>
2022-11-08 14:36:19 -0500

    cleanup deck css

--- projects/client/src/app/views/game-view/deck/deck.component.ts
+++ projects/client/src/app/views/game-view/deck/deck.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { SubscriptionManager } from '../../../util/subscription-manager';
 import { ResponsiveSizeService } from '../responsive-size.service';
 
 @Component({
@@ -11,11 +12,49 @@ export class DeckComponent implements OnChanges {
   @Input() totalNumCards: number;
   @Input() rightSide: boolean;
 
-  numCardsToDisplay = 0;
+  private sm = new SubscriptionManager();
 
-  constructor(public responsiveSizeService: ResponsiveSizeService) {}
+  numCardsToDisplay = 0;
+  cardSize = 64;
+
+  constructor(public responsiveSizeService: ResponsiveSizeService) {
+    this.sm.add(
+      responsiveSizeService.cardSize$.subscribe((cardSize) => {
+        this.cardSize = cardSize;
+      })
+    );
+  }
 
   ngOnChanges() {
     this.numCardsToDisplay = this.totalNumCards >= 3 ? 3 : this.totalNumCards;
+  }
+
+  getCardCountFontSize() {
+    const pixels = Math.min(this.cardSize / 4, 22);
+    return pixels + 'px';
+  }
+
+  getCardCountTop() {
+    const pixels = this.cardSize + 4;
+    return pixels + 'px';
+  }
+
+  getCardRight(cardIndex: number) {
+    let pixels = 0;
+
+    if (this.rightSide) {
+      const slightCardShift = cardIndex * 6;
+      const halfCardShift = this.cardSize / 2;
+      const boardAndDeckPadding = this.cardSize / 6;
+      const totalShift = slightCardShift + halfCardShift + boardAndDeckPadding;
+
+      pixels = -totalShift;
+    } else {
+      const cardShift = cardIndex * 6;
+
+      pixels = cardShift;
+    }
+
+    return pixels + 'px';
   }
 }
