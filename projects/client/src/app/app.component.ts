fac358f34363d785bb6687e0d82733fab4a076bd
jamesworden <jamesworden.ny@gmail.com>
2022-11-10 12:12:39 -0500

    animation between views

--- projects/client/src/app/app.component.ts
+++ projects/client/src/app/app.component.ts
@@ -3,11 +3,21 @@ import { Select } from '@ngxs/store';
 import { ViewState } from './state/view.state';
 import { View } from './views';
 import { Observable } from 'rxjs';
+import { style, animate, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate(500, style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class AppComponent {
   @Select(ViewState.currentView)
