739bcc55546607ad63bb2b9ee3e78ce963386da4
jamesworden <jamesworden.ny@gmail.com>
2023-03-03 17:27:00 -0500

    Add resign button and menu

--- projects/client/src/app/views/game-view/components/modal/modal.component.ts
+++ projects/client/src/app/views/game-view/components/modal/modal.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject, Output, EventEmitter } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Store } from '@ngxs/store';
+import { ModalData } from './modal-data';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  @Output() buttonClicked = new EventEmitter<string>();
+
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
+
     @Inject(MAT_DIALOG_DATA)
-    public data: {
-      header: string;
-      message: string;
-    }
+    public data: ModalData
   ) {}
 
-  ngOnInit(): void {}
+  onButtonClicked(button: string) {
+    this.buttonClicked.emit(button);
+  }
 }
--- projects/client/src/app/views/game-view/components/sidebar/sidebar.component.ts
+++ projects/client/src/app/views/game-view/components/sidebar/sidebar.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ResponsiveSizeService } from '../../services/responsive-size.service';
+import { ModalData } from '../modal/modal-data';
+import { ModalComponent } from '../modal/modal.component';
+
+enum ResignModalButtons {
+  Yes = 'Yes',
+  No = 'No',
+}
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +15,31 @@ import { ResponsiveSizeService } from '../../services/responsive-size.service';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  constructor(public responsiveSizeService: ResponsiveSizeService) {}
+  constructor(
+    public responsiveSizeService: ResponsiveSizeService,
+    public modal: MatDialog
+  ) {}
+
+  onResign() {
+    const modalData: ModalData = {
+      message: 'Are you sure you want to resign?',
+      buttons: [ResignModalButtons.Yes, ResignModalButtons.No],
+    };
+    const modalRef = this.modal.open(ModalComponent, {
+      width: '250px',
+      data: modalData,
+    });
+
+    modalRef.componentInstance.buttonClicked.subscribe((selectedOption) => {
+      if (selectedOption === ResignModalButtons.No) {
+        modalRef.close();
+      } else if (selectedOption === ResignModalButtons.Yes) {
+        console.log('resign');
+      }
+    });
+
+    modalRef.afterClosed().subscribe(() => {
+      modalRef.componentInstance.buttonClicked.unsubscribe();
+    });
+  }
 }
