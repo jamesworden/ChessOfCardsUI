f22cb9918718dacdb109186b49889b577d6eb6bd
jamesworden <jamesworden.ny@gmail.com>
2023-03-05 02:31:05 -0500

    remove side decks

--- projects/client/src/app/app.module.ts
+++ projects/client/src/app/app.module.ts
@@ -24,12 +24,10 @@ import { TutorialButtonComponent } from './views/tutorial-view/components/tutori
 import { TutorialState } from './state/tutorial.state';
 import { CardComponent } from './views/game-view/components/card/card.component';
 import { FaceDownCardComponent } from './views/game-view/components/face-down-card/face-down-card.component';
-import { DeckComponent } from './views/game-view/components/deck/deck.component';
 import { PositionComponent } from './views/game-view/components/position/position.component';
 import { ModalComponent } from './views/game-view/components/modal/modal.component';
 import { PlaceMultipleCardsLaneComponent } from './views/game-view/components/place-multiple-cards-lane/place-multiple-cards-lane.component';
 import { LaneComponent } from './views/game-view/components/lane/lane.component';
-import { GutterComponent } from './views/game-view/components/gutter/gutter.component';
 import { PlayerHandComponent } from './views/game-view/components/player-hand/player-hand.component';
 import { OpponentHandComponent } from './views/game-view/components/opponent-hand/opponent-hand.component';
 import { BoardComponent } from './views/game-view/components/board/board.component';
@@ -46,7 +44,6 @@ import { ServerState } from './state/server.state';
     JoinViewComponent,
     FaceDownCardComponent,
     CardComponent,
-    DeckComponent,
     PositionComponent,
     ModalComponent,
     PlaceMultipleCardsLaneComponent,
@@ -54,7 +51,6 @@ import { ServerState } from './state/server.state';
     TutorialViewComponent,
     TutorialButtonComponent,
     LaneComponent,
-    GutterComponent,
     PlayerHandComponent,
     OpponentHandComponent,
     BoardComponent,
