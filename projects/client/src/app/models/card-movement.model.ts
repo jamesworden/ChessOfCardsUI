47f7365cd55ddd507bc74f9b5faa45d3f733031c
jamesworden <jamesworden.ny@gmail.com>
2023-12-05 07:23:07 -0500

    If no card in cardMovement array, return face down card

--- projects/client/src/app/models/card-movement.model.ts
+++ projects/client/src/app/models/card-movement.model.ts
@@ -2,7 +2,7 @@ import { CardStore } from './card-store.model';
 import { Card } from './card.model';
 
 export type CardMovement = {
-  From?: CardStore;
-  To?: CardStore;
-  Card: Card;
+  From: CardStore;
+  To: CardStore;
+  Card?: Card;
 };
