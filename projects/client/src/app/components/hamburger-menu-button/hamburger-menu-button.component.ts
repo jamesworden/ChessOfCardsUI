7009798634406f133053578f8663feac8e21905f
jamesworden <jamesworden.ny@gmail.com>
2024-02-28 12:35:40 -0500

    More hamburger menu things

--- projects/client/src/app/components/hamburger-menu-button/hamburger-menu-button.component.ts
+++ projects/client/src/app/components/hamburger-menu-button/hamburger-menu-button.component.ts
@@ -4,6 +4,8 @@ import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { NavbarState } from '../../state/navbar.state';
 import { fadeInOutAnimation } from '../../animations/fade-in-out.animation';
+import { DEMO_PLAYER_GAME_VIEW } from '../../views/home-view/data/demo-player-game-view';
+import { ResponsiveSizeService } from '../../views/game-view/services/responsive-size.service';
 
 @Component({
   selector: 'app-hamburger-menu-button',
@@ -12,10 +14,15 @@ import { fadeInOutAnimation } from '../../animations/fade-in-out.animation';
   animations: [fadeInOutAnimation],
 })
 export class HamburgerMenuButtonComponent {
+  readonly DEMO_PLAYER_GAME_VIEW = DEMO_PLAYER_GAME_VIEW;
+
   readonly #store = inject(Store);
+  readonly #responsiveSizeService = inject(ResponsiveSizeService);
 
   @Select(NavbarState.isOpen)
   isOpen$!: Observable<boolean>;
+
+  readonly cardSize$ = this.#responsiveSizeService.cardSize$;
 
   toggleHamburgerMenu() {
     this.#store.dispatch(new ToggleNavbar());
