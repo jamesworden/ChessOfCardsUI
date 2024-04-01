91b02d26dec2d7317ca2a2e522f7c74a27abc06e
jamesworden <jamesworden.ny@gmail.com>
2024-04-01 10:43:45 -0400

    More CSS Changes

--- projects/client/src/app/views/home-view/home-view.component.ts
+++ projects/client/src/app/views/home-view/home-view.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { DEMO_PLAYER_GAME_VIEW } from '../../metadata/demo-player-game-view';
 import { ResponsiveSizeService } from '../game-view/services/responsive-size.service';
 import { Router } from '@angular/router';
 
@@ -9,10 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./home-view.component.scss'],
 })
 export class HomeViewComponent {
-  readonly DEMO_PLAYER_GAME_VIEW = DEMO_PLAYER_GAME_VIEW;
-
   readonly #responsiveSizeService = inject(ResponsiveSizeService);
   readonly #router = inject(Router);
 
   readonly cardSize$ = this.#responsiveSizeService.cardSize$;
+
+  scrollToPlayNow() {
+    const playNowSection = document.getElementById('play-now-section');
+    playNowSection?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
 }
