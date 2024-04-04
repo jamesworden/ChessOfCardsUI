7d56e5d48cb6cd8dbcc4bd07af82d5e3707b92ca
jamesworden <jamesworden.ny@gmail.com>
2024-04-04 12:19:25 -0400

    Extract animations from animation overlay file to smaller files

--- projects/client/src/app/views/game-view/components/animation-overlay/animation-overlay.component.ts
+++ projects/client/src/app/views/game-view/components/animation-overlay/animation-overlay.component.ts
@@ -4,174 +4,35 @@ import {
   Input,
   Output,
   OnInit,
-  OnDestroy,
+  inject,
+  DestroyRef
 } from '@angular/core';
 import { Z_INDEXES } from '../../z-indexes';
 import { AnimatedEntity } from './models/animated-entity.model';
 import { BehaviorSubject } from 'rxjs';
-import {
-  animate,
-  keyframes,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
-import { SubscriptionManager } from 'projects/client/src/app/util/subscription-manager';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AnimationType } from './models/animation-type.model';
 import { getSequencesToDelayMs } from './logic/get-sequences-to-delay-ms';
 import { map } from 'rxjs/operators';
+import { cardMovementAnimation } from './animations/card-movement.animation';
+import { fadeOutAnimation } from './animations/fade-out.animation';
+import { fadeInAnimation } from './animations/fade-in.animation';
 
 @Component({
   selector: 'app-animation-overlay',
   templateUrl: './animation-overlay.component.html',
   styleUrl: './animation-overlay.component.scss',
   animations: [
-    trigger('cardMovement', [
-      state(
-        'todo',
-        // Off screen hack
-        style({
-          top: '-100px',
-          left: '-100px',
-        })
-      ),
-      state(
-        'completed',
-        style({
-          top: '{{ toY }}px',
-          left: '{{ toX }}px',
-        }),
-        {
-          params: {
-            toX: 0,
-            toY: 0,
-          },
-        }
-      ),
-      transition(
-        '* => in-progress',
-        [
-          animate(
-            '{{ durationMs }}ms ease',
-            keyframes([
-              style({ top: '{{ fromY }}px', left: '{{ fromX }}px' }),
-              style({ top: '{{ toY }}px', left: '{{ toX }}px' }),
-            ])
-          ),
-          style({
-            opacity: 1,
-          }),
-        ],
-        {
-          params: {
-            toX: 0,
-            toY: 0,
-            fromX: 0,
-            fromY: 0,
-            durationMs: 0,
-          },
-        }
-      ),
-    ]),
-    trigger('fadeOut', [
-      state(
-        'todo',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 1,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      state(
-        'in-progress',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 0,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      state(
-        'completed',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 0,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      transition('* => in-progress', animate('{{durationMs}}ms ease-in-out')),
-    ]),
-
-    trigger('fadeIn', [
-      state(
-        'todo',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 0,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      state(
-        'in-progress',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 1,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      state(
-        'completed',
-        style({
-          top: '{{y}}px',
-          left: '{{x}}px',
-          opacity: 1,
-        }),
-        {
-          params: {
-            x: 0,
-            y: 0,
-          },
-        }
-      ),
-      transition('* => in-progress', animate('{{durationMs}}ms ease-in-out')),
-    ]),
+    cardMovementAnimation,
+    fadeOutAnimation,
+    fadeInAnimation
   ],
 })
-export class AnimationOverlayComponent implements OnInit, OnDestroy {
-  private readonly sm = new SubscriptionManager();
-
+export class AnimationOverlayComponent implements OnInit {
   readonly Z_INDEXES = Z_INDEXES;
   readonly AnimationType = AnimationType;
+
+  readonly #destroyRef = inject(DestroyRef);
 
   @Input({ required: true }) set animatedEntities(
     animatedEntities: AnimatedEntity<unknown>[]
@@ -206,15 +67,9 @@ export class AnimationOverlayComponent implements OnInit, OnDestroy {
   readonly currentSequence$ = new BehaviorSubject<number | null>(null);
 
   ngOnInit() {
-    this.sm.add(
-      this.sequencesWithDelays$.subscribe((sequencesWithDelays) =>
+    this.sequencesWithDelays$.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe((sequencesWithDelays) =>
         this.updateCurrentSequence(sequencesWithDelays)
       )
-    );
-  }
-
-  ngOnDestroy() {
-    this.sm.unsubscribe();
   }
 
   updateCurrentSequence(
