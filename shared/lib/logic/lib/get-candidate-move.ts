dff76e82f7aa1020c1aca27bdad3e4bffdbc9a66
jamesworden <jamesworden.ny@gmail.com>
2024-04-13 10:37:54 -0400

    Changes

--- shared/lib/logic/lib/get-candidate-move.ts
+++ shared/lib/logic/lib/get-candidate-move.ts
@@ -1,37 +1,18 @@
 import { CandidateMove, Move } from '@shared/models';
 
 export function getCandidateMove(move: Move, candidateMoves?: CandidateMove[]) {
-  return candidateMoves?.find((candidateMove) => {
-    if (move.PlaceCardAttempts.length === 0) {
-      return null;
-    }
-
-    const matchingNumAttempts =
-      candidateMove.Move.PlaceCardAttempts.length ===
-      move.PlaceCardAttempts.length;
-
-    if (!matchingNumAttempts) {
-      return null;
-    }
-
-    for (const candidateAttempt of candidateMove.Move.PlaceCardAttempts) {
-      for (const attempt of move.PlaceCardAttempts) {
-        if (attempt.Card.Kind !== candidateAttempt.Card.Kind) {
-          continue;
-        }
-        if (attempt.Card.Suit !== candidateAttempt.Card.Suit) {
-          continue;
-        }
-        if (attempt.TargetLaneIndex !== candidateAttempt.TargetLaneIndex) {
-          continue;
-        }
-        if (attempt.TargetRowIndex !== candidateAttempt.TargetRowIndex) {
-          continue;
-        }
-        return true;
-      }
-    }
-
-    return false;
-  });
+  return candidateMoves?.find(
+    (candidateMove) =>
+      move.PlaceCardAttempts.length ===
+        candidateMove.Move.PlaceCardAttempts.length &&
+      candidateMove.Move.PlaceCardAttempts.every((candidateAttempt) =>
+        move.PlaceCardAttempts.some(
+          (attempt) =>
+            attempt.Card.Kind === candidateAttempt.Card.Kind &&
+            attempt.Card.Suit === candidateAttempt.Card.Suit &&
+            attempt.TargetLaneIndex === candidateAttempt.TargetLaneIndex &&
+            attempt.TargetRowIndex === candidateAttempt.TargetRowIndex
+        )
+      )
+  );
 }
