cca71c997095da3fbab70050f5e3cb85b5110fe1
jamesworden <jamesworden.ny@gmail.com>
2024-05-15 14:23:50 -0400

    Improvements to moves panel

--- projects/client/src/app/views/game-view/components/game-view-navbar/game-view-navbar.component.ts
+++ projects/client/src/app/views/game-view/components/game-view-navbar/game-view-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { GameViewTab } from '../../models/game-view-tab';
+import { Z_INDEXES } from '@shared/constants';
 
 @Component({
   selector: 'app-game-view-navbar',
@@ -12,6 +13,7 @@ export class GameViewNavbarComponent {
   @Output() tabSelected = new EventEmitter<GameViewTab>();
 
   readonly GameViewTab = GameViewTab;
+  readonly Z_INDEXES = Z_INDEXES;
 
   readonly gameViewTabs: {
     iconClass: string;
