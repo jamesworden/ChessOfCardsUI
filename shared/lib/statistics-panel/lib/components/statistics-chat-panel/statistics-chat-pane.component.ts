5bf8dd47428cb4804ceac53fcc4c2222a051acf6
jamesworden <jamesworden.ny@gmail.com>
2024-05-15 08:12:38 -0400

    Styles

--- shared/lib/statistics-panel/lib/components/statistics-chat-panel/statistics-chat-pane.component.ts
+++ shared/lib/statistics-panel/lib/components/statistics-chat-panel/statistics-chat-pane.component.ts
@@ -1,6 +1,14 @@
-import { Component, inject, EventEmitter, Output, Input } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { ChatMessage, PlayerOrNone } from '@shared/models';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface DisplayChatMessage {
+  name?: string;
+  messages: string[];
+  date: Date;
+  sentByPlayer: boolean;
+}
 
 @Component({
   selector: 'statistics-chat-pane',
@@ -8,14 +16,51 @@ import { BehaviorSubject } from 'rxjs';
   styleUrl: './statistics-chat-pane.component.scss',
 })
 export class StatisticsChatPaneComponent {
-  @Input({ required: true }) isHost: boolean;
+  @Input({ required: true }) set isHost(isHost: boolean) {
+    this.isHost$.next(isHost);
+  }
   @Input({ required: true }) set chatMessages(chatMessages: ChatMessage[]) {
     this.chatMessages$.next(chatMessages);
   }
 
   @Output() chatMessageSent = new EventEmitter<string>();
 
+  readonly isHost$ = new BehaviorSubject<boolean>(false);
   readonly chatMessages$ = new BehaviorSubject<ChatMessage[]>([]);
+
+  readonly displayedChatMessages$ = combineLatest([
+    this.chatMessages$,
+    this.isHost$,
+  ]).pipe(
+    map(([chatMessages, isHost]) => {
+      const displayChatMessages: DisplayChatMessage[] = [];
+
+      let latestedSentBy = PlayerOrNone.None;
+
+      for (const chatMessage of chatMessages) {
+        if (chatMessage.SentBy === latestedSentBy) {
+          displayChatMessages[displayChatMessages.length - 1].date =
+            chatMessage.SentAtUTC;
+          displayChatMessages[displayChatMessages.length - 1].messages.push(
+            chatMessage.Message
+          );
+        } else {
+          const sentByPlayer =
+            (isHost && chatMessage.SentBy === PlayerOrNone.Host) ||
+            (!isHost && chatMessage.SentBy === PlayerOrNone.Guest);
+
+          displayChatMessages.push({
+            messages: [chatMessage.Message],
+            name: sentByPlayer ? undefined : 'Opponent',
+            date: chatMessage.SentAtUTC,
+            sentByPlayer,
+          });
+        }
+      }
+
+      return displayChatMessages;
+    })
+  );
 
   message = '';
 
@@ -27,7 +72,7 @@ export class StatisticsChatPaneComponent {
     const chatMessage: ChatMessage = {
       Message: this.message,
       SentAtUTC: new Date(),
-      SentBy: this.isHost ? PlayerOrNone.Host : PlayerOrNone.Guest,
+      SentBy: this.isHost$.getValue() ? PlayerOrNone.Host : PlayerOrNone.Guest,
     };
 
     const existingChatMessages = this.chatMessages$.getValue();
