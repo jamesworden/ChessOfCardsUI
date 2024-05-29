d073776421c02df8c3a890525f2e32cade3a2f57
jamesworden <jamesworden.ny@gmail.com>
2024-05-29 13:03:22 -0400

    add comment

--- projects/client/src/app/views/game-view/models/game-view-tab.ts
+++ projects/client/src/app/views/game-view/models/game-view-tab.ts
@@ -1,3 +1,8 @@
+/**
+ * On mobile, these different tabs should all be viable options for selection.
+ * On Desktop, only the `BoardWithStatsPanel` should be rendered. This way, the
+ * other 'tabs' can be selected within the rendered "stats panel".
+ */
 export enum GameViewTab {
   NewGame = 'new-game',
   Moves = 'moves',
