d323c2c698de52f599a879d69a97b0663ed4bbfb
jamesworden <jamesworden.ny@gmail.com>
2024-05-29 13:41:33 -0400

    Added name input

--- shared/lib/statistics-panel/lib/components/statistics-new-game-pane/statistics-new-game-pane.component.ts
+++ shared/lib/statistics-panel/lib/components/statistics-new-game-pane/statistics-new-game-pane.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class StatisticsNewGamePaneComponent {
   joinGameSelected = true;
+  name = '';
 
   selectJoinGame() {
     this.joinGameSelected = true;
