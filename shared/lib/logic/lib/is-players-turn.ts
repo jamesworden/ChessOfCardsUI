394b9c28533b1d6e0d768e65f9dc7fda7e1a4406
jamesworden <jamesworden.ny@gmail.com>
2024-06-25 15:34:09 -0400

    simplify logic

--- shared/lib/logic/lib/is-players-turn.ts
+++ shared/lib/logic/lib/is-players-turn.ts
@@ -1,10 +1,5 @@
 import { PlayerGameView } from '@shared/models';
 
 export function isPlayersTurn(playerGameView: PlayerGameView) {
-  const { isHost: IsHost, isHostPlayersTurn: IsHostPlayersTurn } =
-    playerGameView;
-
-  const hostAndHostTurn = IsHost && IsHostPlayersTurn;
-  const guestAndGuestTurn = !IsHost && !IsHostPlayersTurn;
-  return hostAndHostTurn || guestAndGuestTurn;
+  return playerGameView.isHost && playerGameView.isHostPlayersTurn;
 }
